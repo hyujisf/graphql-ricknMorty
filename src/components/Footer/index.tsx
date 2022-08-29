1177c1ccde308a89e95b6b5436ca892e9fe51c44
hyujisf <canqalloid.me@gmail.com>
2022-08-29 13:50:25 +0700

    fix comp ts btn props

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,15 +1,16 @@
 import { Button } from "@components/Button"
 import "./index.scss"
 export default () => {
+	const btnProps = {
+		className: "nav-link ",
+		type: "link",
+		isExternal: true,
+		href: "https://github.com/hyujisf",
+	}
 	return (
 		<footer className='container'>
 			<div className='footer rounded-top bg-base-100 font-weight-bolder py-3 px-5'>
-				<Button
-					className='nav-link '
-					type='link'
-					isExternal
-					href='https://github.com/hyujisf'
-				>
+				<Button {...btnProps}>
 					Github - Hyujisf <small>(Rick and Morty - Graphql Project)</small>
 				</Button>
 			</div>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,18 +4,18 @@ import { Link } from "react-router-dom"
 import "./index.scss"
 
 export default () => {
+	const btnProps = {
+		className:
+			"btn btn-primary btn-sm rounded-pill nav-link py-1 px-4 font-weight-bolder",
+		isPrimary: true,
+		type: "link",
+		href: "/",
+	}
 	const isDarkTheme = useCurrentTheme()
 	return (
 		<header className='container'>
 			<div className='navbar rounded-bottom bg-base-100 pt-4 pb-3'>
-				<Button
-					className='btn btn-primary btn-sm rounded-pill nav-link py-1 px-4 font-weight-bolder'
-					isPrimary
-					type='link'
-					href='/'
-				>
-					Home
-				</Button>
+				<Button {...btnProps}>Home</Button>
 				<div className=''>
 					<small className='mr-2'>Current System Theme: </small>
 					<span className='bg-base-200 rounded-pill font-weight-bolder py-1 px-3'>
